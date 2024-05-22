c7977ad9a4990be9c5d338586d3506daddb4fba4
henrotanjung <henrotanjung@gmail.com>
2024-05-22 15:26:19 +0700

    uupdate html description and odoo name

--- vendor_portal_sign/__manifest__.py
+++ vendor_portal_sign/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'purchase_rfp_online',
+    'name' : 'Odoo Purchase Vendor Signature',
     'version' : '1.0',
     'summary': 'Provide a feature for vendor to priview and sign',
     'sequence': 1,
